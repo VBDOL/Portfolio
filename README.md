48edd0528988f534ee7c24f23832d47d05849f1e
VBDOL. DEV <155402702+VBDOL@users.noreply.github.com>
2024-10-31 21:43:45 -0300

    Primeiro commit - envio do projeto

---
+++ README.md
@@ -0,0 +1,29 @@
+# Portifólio VBDOL. DEV 
+<div id='top'></div>
+
+Hey, devs!
+
+Neste repositório você encontrará uma página dedicada a lhes apresentar um pouco dos meus trabalhos como desenvolvedor Front End.
+Espero que cada projeto que visualizarem, lhes agradem de alguma forma! 
+
+
+
+
+
+<!-- prettier-ignore -->
+| VBDOL DEV |   |
+| -------------- | -------------------------------------------------------------------------------------------------------------------------------------------------------------------------- |
+| ✨ Nome        | **Portifólio** |
+| 🏷️ Tecnologias |HTML, CSS, JavaScript, React |
+
+<img src="/assets/images/port.JPG" alt="Descrição da imagem">
+
+
+## Detalhes do projeto
+
+<div>
+  <img src="https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white">
+  <img src="https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white">
+  <img src="https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black">
+    
+</div>
---
+++ assets/js/scripts.js
@@ -0,0 +1,40 @@
+/*
+DESENVOLVEDOR: VBDOL.DEV
+GITHUB: https://github.com/VBDOL
+LINKEDIN: https://www.linkedin.com/in/victor-b-o-leme-dev
+Alura: https://cursos.alura.com.br/user/vbdol-dev
+DIO: https://www.dio.me/users/vbdol_dev
+
+DATA: 30/10/2024
+*/
+const toggleTheme = document.getElementById("toggleTheme");
+const rootHtml = document.documentElement
+const accordionHeaders = document.querySelectorAll(".accordion__header");
+const menuLinks = document.querySelectorAll(".menu__link");
+
+function changeTheme(){
+  const currentTheme = rootHtml.getAttribute("data-theme");
+
+  currentTheme === "dark" ? rootHtml.setAttribute("data-theme", "light") : rootHtml.setAttribute("data-theme", "dark")
+
+  toggleTheme.classList.toggle("bi-sun")
+  toggleTheme.classList.toggle("bi-moon-stars")
+}
+
+toggleTheme.addEventListener("click", changeTheme);
+
+accordionHeaders.forEach(header => {
+  header.addEventListener("click", () => {
+    const accordionItem = header.parentElement;
+    const accordionActive = accordionItem.classList.contains("active");
+
+    accordionActive ? accordionItem.classList.remove("active") : accordionItem.classList.add("active");
+  })
+})
+
+menuLinks.forEach(item => {
+  item.addEventListener("click", () => {
+    menuLinks.forEach(i => i.classList.remove("active"));
+    item.classList.add("active");
+  })
+})
